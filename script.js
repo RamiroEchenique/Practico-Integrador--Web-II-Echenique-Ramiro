66caa57fcd733b1d4f27800f66802119bf6a54d1
Ing. Ramiro Echenique <ra.echenique@gmail.com>
2024-09-11 14:05:24 -0300

    se agrego imagen a las que no tienen
    prueba de las primeras 40 obras de arte
    se agrego una imagen referencial para las que no tienen imagen principal

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
 const boton = document.getElementById('botonMostrarObras');
 const div = document.getElementById('obras');
 
-/*
-Este código hace lo siguiente:
 
-Solicita una lista de IDs de objetos del Met Museum.
-Toma los primeros 20 IDs de esa lista.
-Hace solicitudes paralelas para obtener los detalles de cada uno de esos 20 objetos.
-Una vez que tiene todos los detalles, llama a renderObjetos para mostrar los objetos en la página.
-*/ 
-
-//
 const sitioBase = 'https://collectionapi.metmuseum.org/public/collection/v1/';
 
 boton.addEventListener('click', () => {
-    console.log('Fetch API');
-    //fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects') 
-    fetch(`${sitioBase}objects?hasImages=true`) // solicita una lista de IDs de objetos del museo con imagenes hasImages=true
-        .then((response) => response.json()) // respuesta de la API en formato JSON
-        .then((data) => {
-            const objectIDs = data.objectIDs.slice(0, 40); // obtiene los primeros 20 objetos IDs de la lista
-            return Promise.all(objectIDs.map(id => fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`).then(res => res.json()))); // para cada ID en objectIDs, hace una solicitud a la API para obtener el objeto correspondiente
+    //fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=11&hasImages=true')
+    //fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=11')
+    fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects') // devuelvo los objetos ordenados por ID
+        .then(response => response.json())
+        .then(data => {
+            console.log("data: ",data);
+            const objectIDs = data.objectIDs.slice(0, 40); // Obtener los primeros 20 IDs
+            console.log("objectIDs: ",objectIDs); 
+            const promises = objectIDs.map(id => fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`).then(res => res.json()));
+            return Promise.all(promises);
         })
-        .then((objetos) => renderObjetos(objetos)); // El arreglo de objetos se pasa como argumento a renderObjetos para renderizar las tarjetas
+        .then(objects => renderObjetos(objects))
+        //.catch(error => console.error('Error:', error));
 });
 
 function renderObjetos(objetos) {
     div.innerHTML = ''; // Limpiar el contenido anterior
     objetos.forEach(obj => {
-        //if (obj.primaryImageSmall) { // verifica que el objeto tenga una imagen
-            
-        
+        let imagenMuseo;
+        if (obj.primaryImageSmall) { // verifica que el objeto tenga una imagen
+            imagenMuseo=obj.primaryImageSmall;
+        }else{
+            imagenMuseo='sinImagen.png';
+        }
         const card = document.createElement('div');
         card.className = 'card';
         card.innerHTML = `
-            <img src="${obj.primaryImageSmall}" alt="${obj.title}">
+            <img src="${imagenMuseo}" alt="${obj.title}">
             <h3>ID: ${obj.objectID}</h3>
             <h3>Titulo: ${obj.title}</h3>
             <h4>Cultura: ${obj.culture}</h3>
             <h4>Dinastía: ${obj.dynasty}</h3>
         `;  //<p>${obj.artistDisplayName}</p>
         div.appendChild(card);
-        //} // cierre del if
+        // cierre del if
     });
 }
