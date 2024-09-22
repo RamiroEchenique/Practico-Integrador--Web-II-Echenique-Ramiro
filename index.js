f6f56f4ec2ace51e5563cab26a401808124e4dbb
Ing. Ramiro Echenique <117690034+RamiroEchenique@users.noreply.github.com>
2024-09-22 01:01:39 -0300

    node.js - nodemon - correccion de html y estilos

---
+++ index.js
@@ -0,0 +1,13 @@
+const express    = require('express');
+const app        = express();
+const port       = 3000;
+
+app.use(express.static('public'));
+
+/*app.get('/', (req, res) => {
+    res.send('Hola desde mi servidor express en Google Drive');
+});*/
+
+app.listen(port, () => {
+    console.log(`Servidor express en Google Drive escuchando en el puerto ${port}`);
+});
