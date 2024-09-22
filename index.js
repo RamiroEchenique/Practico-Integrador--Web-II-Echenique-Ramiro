5cf205221d284d58c2324187a0c2e9fc2b3556b8
Ing. Ramiro Echenique <117690034+RamiroEchenique@users.noreply.github.com>
2024-09-22 01:19:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express    = require('express');
 const app        = express();
 const port       = 3000;
 
-/*app.use(express.static('Public'));
+app.use(express.static('Public'));
 
 /*app.get('/', (req, res) => {
     res.send('Hola desde mi servidor express en Google Drive');
 });*/
 
-app.use(express.static(__dirname + "/public/"));
+/*app.use(express.static(__dirname + "/public/"));
 app.get('/', function(request, responce){
     responce.sendFile(path.join(__dirname, 'index.html'));
-})
+})*/
 
 app.listen(port, () => {
     console.log(`Servidor express en Google Drive escuchando en el puerto ${port}`);
